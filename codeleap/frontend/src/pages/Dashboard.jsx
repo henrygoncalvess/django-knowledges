ae1bfa2e07d65cd6d85046833e69849d843e0766
henrygoncalvess <octanebt@gmail.com>
2025-09-09 18:27:23 -0300

    feat: create and use `PostCard` component

--- codeleap/frontend/src/pages/Dashboard.jsx
+++ codeleap/frontend/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer/Footer";
 import Section from "@/components/Section/Section";
 import InputCard from "@/components/InputCard/InputCard";
 import InputUser from "@/components/InputUser/InputUser";
+import PostCard from "@/components/PostCard/PostCard";
 
 function Home() {
   return (
@@ -24,8 +25,23 @@ function Home() {
                 tag={"content"}
               />
             </InputCard>
+            <PostCard
+              title={"My First Post at Codeleap Network"}
+              username={"Victor"}
+              postedAt={"25 minutes ago"}
+              content={
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora magni accusamus optio eius mollitia sunt, hic voluptas delectus quo esse nostrum eum inventore aspernatur. Optio suscipit dolorem iusto! Omnis, expedita."
+              }
+            />
+            <PostCard
+              title={"My First Post at Codeleap Network"}
+              username={"Victor"}
+              postedAt={"25 minutes ago"}
+              content={
+                "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora magni accusamus optio eius mollitia sunt, hic voluptas delectus quo esse nostrum eum inventore aspernatur. Optio suscipit dolorem iusto! Omnis, expedita."
+              }
+            />
           </Section>
-          <div className={styles["test"]}></div>
           <Footer />
         </div>
       </div>
