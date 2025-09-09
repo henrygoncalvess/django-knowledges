055fdf861bdbf9c1a3f6d51d4e542ee5379eb0ed
henrygoncalvess <octanebt@gmail.com>
2025-09-09 19:55:07 -0300

    feat: fetching data from backend

--- codeleap/frontend/src/components/PostCard/PostCard.jsx
+++ codeleap/frontend/src/components/PostCard/PostCard.jsx
@@ -1,5 +1,38 @@
 import styles from "@/components/PostCard/postcard.module.css";
 
+function timeAgo(dateString) {
+  const now = new Date();
+  const past = new Date(dateString);
+
+  const diffInSeconds = Math.floor((now - past) / 1000);
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} second${diffInSeconds !== 1 ? "s" : ""} ago`;
+  }
+
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} minute${diffInMinutes !== 1 ? "s" : ""} ago`;
+  }
+
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} hour${diffInHours !== 1 ? "s" : ""} ago`;
+  }
+
+  const diffInDays = Math.floor(diffInHours / 24);
+  if (diffInDays < 30) {
+    return `${diffInDays} day${diffInDays !== 1 ? "s" : ""} ago`;
+  }
+
+  const diffInMonths = Math.floor(diffInDays / 30);
+  if (diffInMonths < 12) {
+    return `${diffInMonths} month${diffInMonths !== 1 ? "s" : ""} ago`;
+  }
+
+  const diffInYears = Math.floor(diffInMonths / 12);
+  return `${diffInYears} year${diffInYears !== 1 ? "s" : ""} ago`;
+}
+
 function PostCard({ title, username, postedAt, content }) {
   return (
     <div className={styles["postCard"]}>
@@ -15,7 +48,7 @@ function PostCard({ title, username, postedAt, content }) {
       <div className={styles["section"]}>
         <menu className={styles["menu"]}>
           <p className={styles["username"]}>@{username}</p>
-          <p className={styles["postedAt"]}>{postedAt}</p>
+          <p className={styles["postedAt"]}>{timeAgo(postedAt)}</p>
         </menu>
         <main className={styles["content"]}>{content}</main>
       </div>
